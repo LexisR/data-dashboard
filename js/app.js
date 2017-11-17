25854d6194dbd3ddca169da6a545fa81430b9eee
LexisR <lexisrodriguez22@gmail.com>
2017-11-17 18:14:35 -0300

    agregando graficos

--- js/app.js
+++ js/app.js
@@ -3,25 +3,25 @@ google.charts.load("current", {packages:["corechart"]});
 google.charts.setOnLoadCallback(drawChart);
 
 function drawChart() {
-  
-          // Create the data table.
-          var data = new google.visualization.DataTable();
-          data.addColumn('string', 'Topping');
-          data.addColumn('number', 'Slices');
-          data.addRows([
-            ['Año', 3],
-            ['Onions', 1],
-            ['Olives', 1],
-            ['Zucchini', 1],
-            ['Pepperoni', 2]
-          ]);
-  
-          // Set chart options
-          var options = {'title':'How Much Pizza I Ate Last Night',
-                         'width':400,
-                         'height':300};
-  
-          // Instantiate and draw our chart, passing in some options.
-          var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
-          chart.draw(data, options);
-        }
+// Create the data table.
+  var data = new google.visualization.DataTable();
+      data.addColumn('string', 'Topping');
+      data.addColumn('number', 'Slices');
+      data.addRows([
+        ['2017-G1', 3],
+        ['2017-G2', 1],
+        ['2016-G1', 1],
+        ['2016-G2', 1],
+        ['2015-G1', 2],
+        ['2015-G2', 2],
+        ['2014-G1', 2]
+      ]);
+// Set chart options
+ var options = {'title':'Estudiantes presentes Santiago',
+                'width':400,'height':300,
+                  
+               };
+// Instantiate and draw our chart, passing in some options.
+  var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+      chart.draw(data, options);
+      }
